96792e53b5346df2a0e956d041a39650fdf13c04
Devendra Singh Shekhawat <shekhawat.dev313@gmail.com>
2017-11-28 10:43:22 -0800

    Update ultrasonic.ino

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,21 +1,8 @@
 /*
-    This sketch reads a ultrasonic rangefinder taht is mounted over a conveyor and returns the distance to the closest object in range.
-    To do this, it sends a pulse to the sensor to initiate a reading, then listens for a pulse to return.
-    The length of the returning pulse is proportional to the time of the returning pulse is converted into a distance in mm.
-    The conveyor transports three types of objects (Object 1, 2 and 3).
-    If a object is whithin a specified interval it will be counted as one of the three objects and printed to the serial monitor, with a measure on that specific object.
-    Ther are also 4 LED's in tree different colors (Green, Red, Yellow and white)
-    The will flash in tree different ways (Green ones, Red twice, yellow three times and White constant) when the objects pass.
-
-  The circuit:
+   The circuit:
   - Groove Base shield
   - Grove - Ultrasonic Ranger attached to pin D7.
   - Grove - LED attached to pin 13, pin 8, pin 4 and 10.
-
-  created 09/25 2015
-  by Amalie Secher Rusbjerg
- */
-
 // Declaring variables
 const int pingPin = 7;  // pingPin (sensor) is placed in pin D7
 int range = 0;
